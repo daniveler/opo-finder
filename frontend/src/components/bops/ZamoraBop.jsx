4f778684e3a6b7865fa9d303d055766a43adead3
daniveler <daniel01velerdas@gmail.com>
2024-07-11 17:38:09 +0200

    Fix margin overlap in ZamoraBop component

--- frontend/src/components/bops/ZamoraBop.jsx
+++ frontend/src/components/bops/ZamoraBop.jsx
@@ -3,15 +3,18 @@ import LinkButton from "../buttons/LinkButton"
 const ZamoraBop = ({ bop }) => {
   return (
     <div>
-      <h1 className="text-3xl mb-8">Fecha: {bop.date}</h1 >
+      <h1 className="flex flex-col text-5xl justify-center items-center py-4">
+        BOP ZAMORA
+      </h1>
+      <h2 className="text-2xl mt-2 mb-4">Fecha: {bop.date}</h2>
       <ul>
-        {bop.content.map(anuncio => (
-          <li>
-            <h1 className="text-2xl mb-2 mt-4">{anuncio.subHeader}</h1>
-            <h2 className="text-xl mb-2">{anuncio.organismo}</h2>
-            <p>{anuncio.text}</p>
-            <div className="mt-4 mb-8">
-              <LinkButton style='pdf' link={anuncio.pdfLink}></LinkButton>
+        {bop.content.map((item, index) => (
+          <li key={`bop-zamora-${index}`}>
+            <h1 className="text-2xl mb-2 mt-4">{item.subHeader}</h1>
+            <h2 className="text-xl mb-2">{item.organismo}</h2>
+            <p>{item.text}</p>
+            <div className="flex flex-row items-center justify-center mt-4 mb-8">
+              <LinkButton style='pdf' link={item.pdfLink}></LinkButton>
             </div>
           </li>
         ))}

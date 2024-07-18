c42ed9252a3e50fc9f4a9975579afda423380dd0
daniveler <daniel01velerdas@gmail.com>
2024-07-18 13:37:16 +0200

    Add bocyl web scrapping endpoint in backend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import cors from 'cors'
-import bopsRouter from './controllers/bopsRouter.js'
 import boeRouter from './controllers/boeRouter.js'
+import bocylRouter from './controllers/bocylRouter.js'
+import bopsRouter from './controllers/bopsRouter.js'
 
 const app = express()
 
 app.use(cors())
 
-app.use('/api/bops', bopsRouter)
 app.use('/api/boe', boeRouter)
+app.use('/api/bocyl', bocylRouter)
+app.use('/api/bops', bopsRouter)
 
 export default app
---
+++ backend/controllers/bocylRouter.js
@@ -0,0 +1,96 @@
+import express from 'express'
+import axios from 'axios'
+import { load } from 'cheerio'
+import { parse } from 'date-fns'
+
+const bocylRouter = express.Router()
+
+bocylRouter.get('/', async (req, res) => {
+  if (!req.query.date) {
+    return res.status(400).json({ error: 'Required query param date is missing' })
+  }
+
+  const formattedDate = parse(req.query.date, 'dd/MM/yyyy', new Date())
+
+  if (formattedDate.getDay() === 0 || formattedDate.getDate() === 6 || formattedDate.getDate() > new Date().getDate()) {
+    return res.status(404).json({ error: 'There are no data available for that day' })
+  }
+
+  const url = `https://bocyl.jcyl.es/boletin.do?fechaBoletin=${req.query.date}`
+
+  let response
+
+  try {
+    response = await axios.get(url)
+  }
+  catch (e) {
+    if (e.response) {
+      return res.status(e.response.status).json({ error: e.message })
+    }
+    else if (e.request) {
+      return res.status(500).json({ error: e.request })
+    }
+    else {
+      return res.status(500).json({ error: 'Internal Server Error' })
+    }
+  }
+
+  const $ = load(response.data)
+
+  const results = []
+
+  $('h4.encabezado4, h4.encabezado4_sinlinea').each((index, element) => {
+    const section = $(element).text().trim()
+
+    const departments = []
+
+    let nextElem = $(element).next()
+
+    while (nextElem.length && !nextElem.is('h4.encabezado4, h4.encabezado4_sinlinea, h3.encabezado3_conlinea')) {
+      if (nextElem.is('h5.encabezado6')) {
+        const department = nextElem.text().trim()
+
+        const titles = []
+
+        nextElem = nextElem.next()
+
+        while (nextElem.length && !nextElem.is('h5.encabezado6, h4.encabezado4, h4.encabezado4_sinlinea, h3.encabezado3_conlinea')) {
+          if (nextElem.is('p')) {
+            const title = nextElem.text().trim()
+
+            const links = []
+
+            nextElem.next('ul').find('li a').each((i, e) => {
+              let link = $(e).attr('href')
+
+              if (link.startsWith('html')) {
+                const baseUrl = 'https://bocyl.jcyl.es/'
+                link = baseUrl.concat(link)
+              }
+
+              links.push(link)
+            })
+
+            titles.push({ title, links })
+          }
+          nextElem = nextElem.next()
+        }
+
+        departments.push({ department, titles })
+      }
+      else {
+        nextElem = nextElem.next()
+      }
+    }
+
+    results.push({ section, departments })
+  })
+
+  const filteredResults = req.query.section
+    ? results.filter(r => r.section === req.query.section)
+    : results
+
+  return res.status(200).json(filteredResults)
+})
+
+export default bocylRouter
